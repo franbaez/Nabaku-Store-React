f267d036029ab3e3dec25a01b544ac8cb2708c75
Franco Baez <francombaez96@gmail.com>
2022-10-26 19:42:44 -0300

    1ra entrega Franco Baez

---
+++ src/components/BodyApp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function BodyApp() {
+  return (
+    <div>
+      MAIN
+      <h1>Bienvenido a Nabaku Store!!</h1>
+      <p>
+        Una lorem ipsum con un par de cosasUna lorem ipsum con un par de
+        cosasUna lorem ipsum con un par de cosasUna lorem ipsum con un par de
+        cosasUna lorem ipsum con un par de cosasUna lorem ipsum con un par de
+        cosas
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      Contacto
+      <ul>
+        <li>
+          <a href="http://whatsapp.com">Contactanos por Whatsapp</a>
+        </li>
+        <li>
+          <a href="http://instagram.com">Contactanos por Instagram</a>
+        </li>
+        <li>
+          <a href="http://facebook.com">Contactanos por Facebook</a>
+        </li>
+        <br />
+        <p>DERECHOS RESERVADOS CAMADA 44970 DE CODER @</p>
+      </ul>
+    </div>
+  );
+}
